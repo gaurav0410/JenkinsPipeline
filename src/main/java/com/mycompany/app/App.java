38ba7c10e61d2ac4df998ce34b1c1c3076d0f0a4
EC2 Default User <ec2-user@ip-10-0-0-38.ec2.internal>
2020-03-01 13:52:47 +0000

    reset App.java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World From Gaurav!";
+    private final String message = "Hello World!";
 
     public App() {}
 
