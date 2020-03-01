cdcae3327d0d6b5754758d21eb0a94a1091d7446
EC2 Default User <ec2-user@ip-10-0-0-38.ec2.internal>
2020-03-01 13:58:41 +0000

    Add 'Deliver' stage

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,33 @@
-package com.mycompany.app;
-
-/**
- * Hello world!
- */
-public class App
-{
-
-    private final String message = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
+pipeline {
+    agent {
+        docker {
+            image 'maven:3-alpine'
+            args '-v /root/.m2:/root/.m2'
+        }
     }
-
-    private final String getMessage() {
-        return message;
+    options {
+        skipStagesAfterUnstable()
     }
-
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn -B -DskipTests clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+            post {
+                always {
+                    junit 'target/surefire-reports/*.xml'
+                }
+            }
+        }
+        stage('Deliver') { 
+            steps {
+                sh './jenkins/scripts/deliver.sh' 
+            }
+        }
+    }
 }
